a96fa3f84dabda3a52f5563df8b297fae6cdbc3c
NicoOeser <nicolas.oeser@gmail.com>
2023-10-06 08:29:51 +0200

    add small-chicken

---
+++ models/small-chicken.class.js
@@ -0,0 +1,37 @@
+class SmallChicken extends MovableObject { 
+    y = 355;
+    height = 70;
+    width = 100;
+    IMAGES_WALKING = [
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png', 
+    ];
+
+
+    constructor() {
+        super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 400 + Math.random() * 2000; // Zahl zwischen 200 und 700
+        this.speed = 0.15 + Math.random() * 0.5;
+
+        this.animate();
+    }
+
+
+    /**
+     * animate movement of the chicken && let chicken move to left side of the map
+     */
+    animate() {
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+
+
+}
